34c9632d84ab90942ae263606656a87e1b85277c
Wilson <wilson.mcdade@gmail.com>
2020-01-14 13:02:32 -0600

    Fixed issue #4

--- logger.py
+++ logger.py
@@ -7,7 +7,7 @@ from datetime import datetime
 def logger(route, ip, file, count):
     count += 1
     now = datetime.now()
-    log_time = now.strftime("%m-%d-%H-%M-%s")
+    log_time = now.strftime("%m-%d-%H-%M-%S")
     log = str(str(log_time)+" : "+str(count)+" : "+str(ip)+": "+route+"\n")
     file.write(log)
     file.flush()
