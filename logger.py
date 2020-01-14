e684afc581363816bcfc9d546c4488d74d0acedf
Wilson <wilson.mcdade@gmail.com>
2020-01-14 12:45:18 -0600

    Added time to logs

--- logger.py
+++ logger.py
@@ -2,9 +2,13 @@
 Wilson's logger
 """
 
+from datetime import datetime
+
 def logger(route, ip, file, count):
     count += 1
-    log = str(str(count)+" : "+str(ip)+": "+route+"\n")
+    now = datetime.now()
+    log_time = now.strftime("%m-%d-%H-%M-%s")
+    log = str(str(log_time)+" : "+str(count)+" : "+str(ip)+": "+route+"\n")
     file.write(log)
     file.flush()
     return count
